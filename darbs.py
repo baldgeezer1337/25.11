bf3cb03d5f5f58bf2080de5ee8efba67649f85f0
baldgeezer1337 <nikistarij@gmail.com>
2024-11-25 10:16:34 +0200

    hfh

--- darbs.py
+++ darbs.py
@@ -16,14 +16,29 @@ conn.execute('''CREATE TABLE AUTORS
              UZVARDS       TEXT        NOT NULL);''')
 print('Tabula ir izveidota!')"""
 
-conn.execute("INSERT INTO GRAMATAS (GRAMATAS_ID,NOSAUKUMS,GADS,AUTORA_ID)\
+"""conn.execute("INSERT INTO GRAMATAS (GRAMATAS_ID,NOSAUKUMS,GADS,AUTORA_ID)\
              VALUES(1,'Kolobok',1986,50)");
 
-conn.execute("INSERT INTO COMPANY (GRAMATAS_ID,NOSAUKUMS,GADS,AUTORA_ID)\
+conn.execute("INSERT INTO GRAMATAS (GRAMATAS_ID,NOSAUKUMS,GADS,AUTORA_ID)\
              VALUES(2,'Dezhavju',2019,51)");
 
-conn.execute("INSERT INTO COMPANY (GRAMATAS_ID,NOSAUKUMS,GADS,AUTORA_ID)\
+conn.execute("INSERT INTO GRAMATAS (GRAMATAS_ID,NOSAUKUMS,GADS,AUTORA_ID)\
              VALUES(3,'White Noise',2001,52)");
+
+
+conn.execute("INSERT INTO AUTORS (AUTORA_ID,VARDS,UZVARDS)\
+             VALUES(50,'Igors','Valambaevs')");
+
+conn.execute("INSERT INTO AUTORS (AUTORA_ID,VARDS,UZVARDS)\
+             VALUES(51,'Oleg','Netrureps')");
+
+conn.execute("INSERT INTO AUTORS (AUTORA_ID,VARDS,UZVARDS)\
+             VALUES(52,'Aphex','Twins')");"""
+
+cursor=conn.execute("SELECT * from GRAMATAS")
+for ieraksts in cursor:
+    print("NOSAUKUMS= ", ieraksts[1])
+    print("GADS= ", ieraksts[2],"\n")
 
 
 conn.commit()
